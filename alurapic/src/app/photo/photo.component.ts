f042ba5ede8048360e3594e73334bda92598cb8e
ErickBolorini <erick.bolorini@tjam.jus.br>
2022-06-15 09:40:35 -0400

    aula 1

---
+++ alurapic/src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: 'photo.component.html',
+
+})
+export class PhotoComponent{
+
+  url = 'https://s4.static.brasilescola.uol.com.br/be/2021/04/casal-de-leoes.jpg';
+}
