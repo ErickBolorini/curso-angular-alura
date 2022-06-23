d2b65e4cabdc0f53b7c985c37771ae6e0e13bbf7
ErickBolorini <erick.bolorini@tjam.jus.br>
2022-06-23 13:39:42 -0400

    atualização

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,29 +1,32 @@
-import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   usuario = '';
   senha = '';
 
-  constructor(private authService: AutenticacaoService ) { }
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        this.router.navigate(['animais']);
+      },
+      (error) => {
+        alert('Usuário ou senha inválido');
+        console.log(error);
+      }
+    );
   }
-
-  login(){
-    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
-      console.log("Autenticado com sucesso");
-    }, (error) => {
-      console.log("Usuário ou senha incorretos");
-      console.log(error);
-    });
-
-  }
-
 }
